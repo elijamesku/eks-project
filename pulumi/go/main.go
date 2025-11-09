439119d08b08353a60a5f18a89d0afbb76d8b27f
Eli James <116235057+elijamesku@users.noreply.github.com>
2025-11-08 22:14:01 -0600

    Add converted Pulumi code: main.go

---
+++ pulumi/go/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "github.com/pulumi/pulumi-aws/sdk/v6/go/aws"
+    "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+    pulumi.Run(func(ctx *pulumi.Context) error {
+        // TODO: Convert Terraform resources to Pulumi Go
+        // This is a basic template
+        return nil
+    })
+}
