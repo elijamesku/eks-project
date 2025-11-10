3660d1594300b4539a71291411a9f589a2ed8aa0
Eli James <ejames@lead.bank>
2025-11-09 23:17:14 -0600

    Add infrastructure changes

--- storage.tf
+++ storage.tf
@@ -5,51 +5,16 @@ resource "aws_s3_bucket" "topskys" {
   }
 }
 
-resource "aws_s3_bucket" "main" {
-  bucket = "my-s3-bucket"
-  tags = {
-    Name = "my-s3-bucket"
-  }
-}
-
-resource "aws_s3_bucket" "main" {
-  bucket = "my-s3-bucket"
-  tags = {
-    Name = "my-s3-bucket"
-  }
-}
-
-resource "aws_s3_bucket" "main" {
-  bucket = "my-s3-bucket"
-  tags = {
-    Name = "my-s3-bucket"
-  }
-}
-
-resource "aws_s3_bucket" "main" {
-  bucket = "my-s3-bucket"
-  tags = {
-    Name = "my-s3-bucket"
-  }
-}
-
-resource "aws_s3_bucket" "main" {
-  bucket = "my-s3-bucket"
-  tags = {
-    Name = "my-s3-bucket"
-  }
-}
-
-resource "aws_s3_bucket" "main" {
-  bucket = "my-s3-bucket"
-  tags = {
-    Name = "my-s3-bucket"
-  }
-}
-
 resource "aws_s3_bucket" "vancleee" {
   bucket = "vancleee"
   tags = {
     Name = "vancleee"
+  }
+}
+
+resource "aws_s3_bucket" "main" {
+  bucket = "my-s3-bucket"
+  tags = {
+    Name = "my-s3-bucket"
   }
 }
