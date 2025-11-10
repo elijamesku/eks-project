11b0b4b286b83a4428e9896be180d91e578557c7
Eli James <ejames@lead.bank>
2025-11-09 21:56:32 -0600

    Add infrastructure changes

--- storage.tf
+++ storage.tf
@@ -4,3 +4,38 @@ resource "aws_s3_bucket" "topskys" {
     Name = "topskys"
   }
 }
+
+resource "aws_s3_bucket" "main" {
+  bucket = "my-s3-bucket"
+  tags = {
+    Name = "my-s3-bucket"
+  }
+}
+
+resource "aws_s3_bucket" "main" {
+  bucket = "my-s3-bucket"
+  tags = {
+    Name = "my-s3-bucket"
+  }
+}
+
+resource "aws_s3_bucket" "five" {
+  bucket = "five"
+  tags = {
+    Name = "five"
+  }
+}
+
+resource "aws_s3_bucket" "fire" {
+  bucket = "fire"
+  tags = {
+    Name = "fire"
+  }
+}
+
+resource "aws_s3_bucket" "flash" {
+  bucket = "flash"
+  tags = {
+    Name = "flash"
+  }
+}
