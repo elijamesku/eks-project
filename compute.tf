006ba12bfe033144c417a7125a5490fb9cf826fe
Eli James <ejames@lead.bank>
2025-11-10 19:50:24 -0600

    Add infrastructure changes

--- compute.tf
+++ compute.tf
@@ -42,3 +42,12 @@ resource "aws_instance" "windows_instance" {
     OS = "Windows"
   }
 }
+
+resource "aws_instance" "windows_instance" {
+  ami = "ami-0c02fb55956c7d316"
+  instance_type = "t3.medium"
+  tags = {
+    Name = "Windows Instance"
+    OS = "Windows"
+  }
+}
