f5580c4a407ebd2a2fcc1bd576e387c9dd37d147
Eli James <ejames@lead.bank>
2025-11-12 00:41:02 -0600

    Add infrastructure changes

--- network.tf
+++ network.tf
@@ -1,16 +1,16 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
     Name = "kali-vpc"
   }
 }
 
 resource "aws_subnet" "main" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
   tags = {
     Name = "kali-subnet"
@@ -26,11 +26,13 @@ resource "aws_internet_gateway" "main" {
 
 resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
-  route = [{
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.main.id
-    }]
   tags = {
     Name = "kali-rt"
   }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
 }
--- security.tf
+++ security.tf
@@ -1,17 +1,17 @@
 resource "aws_security_group" "kali" {
-  name = "kali-sg"
+  name        = "kali-sg"
   description = "Security group for Kali Linux instance"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
